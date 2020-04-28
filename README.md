124861366e9d2b383653346584d9e5a445739fa9
James Zhang <zhangxj@huawei.com>
2020-04-28 04:59:57 -0400

    go routine_id vertify.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # goroutine_id
+
+
+## go 1.13 version
+## centos7.6 yum -y install golang 
+
+
+
+
---
+++ go/src/main.go
@@ -0,0 +1,21 @@
+package  main
+
+import (
+  "goid"
+  "fmt"
+)
+
+func main(){
+
+   fmt.Println(" Initializing ")
+   goid.TextGet()
+
+}
+
+
+
+
+
+
+
+
